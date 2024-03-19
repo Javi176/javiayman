7deeaca6d187e426cf35c2f92c672c2ca7beb32d
Javi176 <160005705+Javi176@users.noreply.github.com>
2024-03-19 18:06:36 +0100

    TestReadLine.java

--- practica1/TestReadLine.java
+++ practica1/TestReadLine.java
@@ -1,16 +1,12 @@
 import java.io.*;
-
 class TestReadLine {
-    public static void main(String[] args) {
-        BufferedReader in = new EditableBufferedReader{
-            new InputStreamReader(System.in);
-            String str = null;
-            try{
-                str = in.readLine();
-            } catch(IOException e) 
-            { e.printStackTrace();
-            }
-            System.out.println("\nline is: " + str);
-        }
-    }
+  public static void main(String[] args) {
+    InputStreamReader input=new InputStreamReader(System.in);
+    EditableBufferedReader in = new EditableBufferedReader(input);
+    String str = null ;
+    try {
+      str = in.readLine();
+    } catch (IOException e) { e.printStackTrace(); }
+    System.out.println("\nline is: " + str);
+  }
 }
