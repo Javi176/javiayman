0c8bf9fde5cd2f96fee736bf842aee3e53130295
Javi176 <160005705+Javi176@users.noreply.github.com>
2024-05-07 20:00:25 +0200

    Add files via upload

---
+++ tres-en-raya/servidor.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
+app.use(express.static(__dirname));
+
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+
+});
+
